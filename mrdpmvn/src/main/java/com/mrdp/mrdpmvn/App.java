6a891a16bbf7a1cd548a85ec659cfd5f7901f2ee
analog76 <analog76@home.(none)>
2014-04-24 12:25:20 -0700

    first commit

---
+++ mrdpmvn/src/main/java/com/mrdp/mrdpmvn/App.java
@@ -0,0 +1,13 @@
+package com.mrdp.mrdpmvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
